e5454e66cef11c4d335444eedb412ad75f0249d4
Jonathan Fields <jonathanfields@gmail.com>
2024-02-19 14:21:36 -0500

    fear expression

---
+++ src/unity/facs/engine3d.js
@@ -0,0 +1,7 @@
+export function Engine3D() {
+	//parameters for function : intensity [A-E] ,  left or right [L,R,null] 
+};
+
+Engine3D.prototype = {
+    setTargets : function(targetWeights, smoothTimes){},
+};
