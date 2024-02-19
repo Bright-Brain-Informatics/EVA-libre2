8b19774c1328bc90783053c42a1922936b90a6df
Jonathan Fields <jonathanfields@gmail.com>
2024-02-19 17:14:46 -0500

    stable cloud

--- src/unity/facs/engine3d.js
+++
@@ -1,7 +0,0 @@
-export function Engine3D() {
-	//parameters for function : intensity [A-E] ,  left or right [L,R,null] 
-};
-
-Engine3D.prototype = {
-    setTargets : function(targetWeights, smoothTimes){},
-};
